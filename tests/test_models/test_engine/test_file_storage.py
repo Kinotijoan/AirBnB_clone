22db58cf958596b7de7dc2f8833d98d1d92eac55
Kinotijoan <117837618+Kinotijoan@users.noreply.github.com>
2023-05-16 12:02:34 +0300

    User

--- tests/test_models/test_engine/test_file_storage.py
+++ tests/test_models/test_engine/test_file_storage.py
@@ -1,53 +1,111 @@
 #!/usr/bin/python3
-"""Defines unittests for file_storage.py"""
+"""Defines unittests for models/engine/file_storage.py.
 
-import unittest
-from models.base_model import BaseModel
+Unittest classes:
+    TestFileStorage_instantiation
+    TestFileStorage_methods
+"""
 import os
+import json
 import models
+import unittest
 from datetime import datetime
-from time import sleep
+from models.base_model import BaseModel
+from models.engine.file_storage import FileStorage
 from models.user import User
 from models.state import State
+from models.place import Place
 from models.city import City
 from models.amenity import Amenity
-from models.place import Place
 from models.review import Review
-from models.engine.file_storage import FileStorage
 
 
-class TestFileStorage(unittest.TestCase):
+class TestFileStorage_instantiation(unittest.TestCase):
+    """Unittests for testing instantiation of the FileStorage class."""
+
+    def test_FileStorage_instantiation_no_args(self):
+        self.assertEqual(type(FileStorage()), FileStorage)
+
+    def test_FileStorage_instantiation_with_arg(self):
+        with self.assertRaises(TypeError):
+            FileStorage(None)
+
+    def test_FileStorage_file_path_is_private_str(self):
+        self.assertEqual(str, type(FileStorage._FileStorage__file_path))
+
+    def testFileStorage_objects_is_private_dict(self):
+        self.assertEqual(dict, type(FileStorage._FileStorage__objects))
+
+    def test_storage_initializes(self):
+        self.assertEqual(type(models.storage), FileStorage)
+
+
+class TestFileStorage_methods(unittest.TestCase):
+    """Unittests for testing methods of the FileStorage class."""
+
+    @classmethod
     def setUp(self):
-        self.storage = FileStorage()
+        try:
+            os.rename("file.json", "tmp")
+        except IOError:
+            pass
 
+    @classmethod
     def tearDown(self):
-        if os.path.exists(self.storage._FileStorage__file_path):
-            os.remove(self.storage._FileStorage__file_path)
+        try:
+            os.remove("file.json")
+        except IOError:
+            pass
+        try:
+            os.rename("tmp", "file.json")
+        except IOError:
+            pass
+        FileStorage._FileStorage__objects = {}
 
     def test_all(self):
-        # Test when there are no objects in the storage
-        objects = self.storage.all()
-        self.assertEqual(len(objects), 0)
+        self.assertEqual(dict, type(models.storage.all()))
 
-        # Test when there are objects in the storage
-        obj1 = BaseModel()
-        obj2 = User()
-        self.storage.new(obj1)
-        self.storage.new(obj2)
-        objects = self.storage.all()
-        self.assertEqual(len(objects), 2)
-        self.assertIn(f"BaseModel.{obj1.id}", objects)
-        self.assertIn(f"User.{obj2.id}", objects)
-        self.assertIs(objects[f"BaseModel.{obj1.id}"], obj1)
-        self.assertIs(objects[f"User.{obj2.id}"], obj2)
+    def test_all_with_arg(self):
+        with self.assertRaises(TypeError):
+            models.storage.all(None)
 
     def test_new(self):
-        # Test adding a new object to the storage
-        obj = BaseModel()
-        self.storage.new(obj)
-        objects = self.storage.all()
-        self.assertIn(f"BaseModel.{obj.id}", objects)
-        self.assertIs(objects[f"BaseModel.{obj.id}"], obj)
+        bm = BaseModel()
+        us = User()
+        st = State()
+        pl = Place()
+        cy = City()
+        am = Amenity()
+        rv = Review()
+        models.storage.new(bm)
+        models.storage.new(us)
+        models.storage.new(st)
+        models.storage.new(pl)
+        models.storage.new(cy)
+        models.storage.new(am)
+        models.storage.new(rv)
+        self.assertIn("BaseModel." + bm.id, models.storage.all().keys())
+        self.assertIn(bm, models.storage.all().values())
+        self.assertIn("User." + us.id, models.storage.all().keys())
+        self.assertIn(us, models.storage.all().values())
+        self.assertIn("State." + st.id, models.storage.all().keys())
+        self.assertIn(st, models.storage.all().values())
+        self.assertIn("Place." + pl.id, models.storage.all().keys())
+        self.assertIn(pl, models.storage.all().values())
+        self.assertIn("City." + cy.id, models.storage.all().keys())
+        self.assertIn(cy, models.storage.all().values())
+        self.assertIn("Amenity." + am.id, models.storage.all().keys())
+        self.assertIn(am, models.storage.all().values())
+        self.assertIn("Review." + rv.id, models.storage.all().keys())
+        self.assertIn(rv, models.storage.all().values())
+
+    def test_new_with_args(self):
+        with self.assertRaises(TypeError):
+            models.storage.new(BaseModel(), 1)
+
+    def test_new_with_None(self):
+        with self.assertRaises(AttributeError):
+            models.storage.new(None)
 
     def test_save(self):
         bm = BaseModel()
@@ -109,28 +167,6 @@ class TestFileStorage(unittest.TestCase):
     def test_reload_with_arg(self):
         with self.assertRaises(TypeError):
             models.storage.reload(None)
-
-    def test_classes(self):
-        # Test the classes() method
-        classes = self.storage.classes()
-        self.assertIsInstance(classes, dict)
-        self.assertIn("BaseModel", classes)
-        self.assertIs(classes["BaseModel"], BaseModel)
-        self.assertIn("User", classes)
-        self.assertIs(classes["User"], User)
-
-    def test_attributes(self):
-        # Test the attributes() method
-        attributes = self.storage.attributes()
-        self.assertIsInstance(attributes, dict)
-        self.assertIn("BaseModel", attributes)
-        self.assertIsInstance(attributes["BaseModel"], dict)
-        self.assertIn("id", attributes["BaseModel"])
-        self.assertIs(attributes["BaseModel"]["id"], str)
-        self.assertIn("User", attributes)
-        self.assertIsInstance(attributes["User"], dict)
-        self.assertIn("email", attributes["User"])
-        self.assertIs(attributes["User"]["email"], str)
 
 
 if __name__ == "__main__":
